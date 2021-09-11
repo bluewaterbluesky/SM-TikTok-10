332144dc87e00f1a7d5a7ac14ce38d2456d3e0fe
Harley Lumagui <lumagui.harl0723@gmail.com>
2021-09-11 23:16:32 +0800

    Update Fixes

--- app/Views/fragments/home/footer.php
+++ app/Views/fragments/home/footer.php
@@ -32,7 +32,7 @@
 
         </div>
 
-        <div class="d-flex flex-wrap justify-content-center align-items-center bg-dark">
+        <div class="d-flex flex-wrap justify-content-center align-items-center bg-dark" width="100%">
             <div class="container">
                 <p class="text-white text-center mt-3">&copy; <?= date('Y') ?> SM Supermalls</p>
             </div>
